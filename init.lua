fb156d5d98bb22b524a133c014928f7a5ca21d01
= <mateusito10@gmail.com>
2022-12-07 19:14:07 -0300

    feat(utils): add ReloadConfig command and global function

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+require("jos620.utils")
+
 require("jos620.core.options")
 require("jos620.core.theme")
 require("jos620.core.keymaps")
---
+++ lua/jos620/utils.lua
@@ -0,0 +1,13 @@
+-- Reload configuration
+function _G.ReloadConfig()
+  for name in pairs(package.loaded) do
+    if name:match("^jos620") then
+      package.loaded[name] = nil
+    end
+  end
+
+  dofile(vim.env.MYVIMRC)
+end
+
+vim.cmd("command! ReloadConfig lua ReloadConfig()")
+
