686953cb8f5ceb22101ce1ee6be07529d1e18fa4
= <mateusito10@gmail.com>
2022-12-07 19:03:16 -0300

    chore(core): add vim options

--- lua/jos620/core/options.lua
+++ lua/jos620/core/options.lua
@@ -1,1 +1,40 @@
-print("Options loaded")
+local opt = vim.opt
+
+-- Leader key
+vim.g.mapleader = " "
+
+-- Numbers
+opt.number = true
+opt.relativenumber = true
+
+-- Tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- Line wrapping
+opt.wrap = false
+
+-- Search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Cursor line
+opt.cursorline = true
+
+-- Appearence
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- Backspace
+opt.backspace = "indent,eol,start"
+
+-- Clipboard
+opt.clipboard:append("unnamedplus")
+
+-- Split
+opt.splitright = true
+opt.splitbelow = true
+
