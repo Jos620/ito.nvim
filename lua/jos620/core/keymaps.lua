181f9675b7e02f701e9a79bb6368018f0b36e2dd
= <mateusito10@gmail.com>
2022-12-07 19:47:16 -0300

    feat(keymaps): add split and window keymaps

--- lua/jos620/core/keymaps.lua
+++ lua/jos620/core/keymaps.lua
@@ -17,3 +17,22 @@ set("n", "<Leader>l", "$")
 set("v", ">", ">V")
 set("v", "<", "<V")
 
+-- Split window
+set("n", "<Leader>sv", "<C-w>v")
+set("n", "<Leader>sh", "<C-w>s")
+set("n", "<Leader>se", "<C-w>=")
+set("n", "<Leader>q", ":quit<Return>", { silent = true })
+set("n", "<Leader>c", ":close<Return>", { silent = true })
+
+-- Split resize
+set("n", "<C-up>", "<C-w>+")
+set("n", "<C-down>", "<C-w>-")
+set("n", "<C-right>", "<C-w>>")
+set("n", "<C-left>", "<C-w><")
+
+-- Tabs
+set("n", "<C-t>", ":tabnew<Return>", { silent = true })
+set("n", "<C-x>", ":tabclose<Return>", { silent = true })
+set("n", "L", ":tabn<Return>", { silent = true })
+set("n", "H", ":tabp<Return>", { silent = true })
+
