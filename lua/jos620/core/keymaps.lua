411f8ad8ea7cfcb91782a4e9085d1d8f73295ed9
= <mateusito10@gmail.com>
2022-12-08 00:55:51 -0300

    chore(keymaps): remove unused keymaps

--- lua/jos620/core/keymaps.lua
+++ lua/jos620/core/keymaps.lua
@@ -9,10 +9,6 @@ set("n", "<C-s>", ":write<Return>")
 -- Do not yank with "x"
 set("n", "x", '"_x')
 
--- Navigate to start / end of the file
-set("n", "<Leader>h", "^")
-set("n", "<Leader>l", "$")
-
 -- Line indent with visual
 set("v", ">", ">V")
 set("v", "<", "<V")
