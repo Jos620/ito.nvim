68a418b2fa3941e714ff6be08f6a75505bfd9579
Mateus Ito <mateusito10@gmail.com>
2022-12-19 13:58:06 -0300

    chore(keymaps): remove repeated code

--- lua/jos620/core/keymaps.lua
+++ lua/jos620/core/keymaps.lua
@@ -1,5 +1,7 @@
 local set = vim.keymap.set
 
+local opts = { silent = true }
+
 -- Exit insert mode
 set("i", "jk", "<C-[>")
 
@@ -10,7 +12,7 @@ set("n", "<C-s>", ":write<Return>")
 set("n", "x", '"_x')
 
 -- Reload configuration
-set("n", "<Leader>r", ":ReloadConfig<Return>", { silent = true })
+set("n", "<Leader>r", ":ReloadConfig<Return>", opts)
 
 -- Line indent with visual
 set("v", ">", ">V")
@@ -20,8 +22,8 @@ set("v", "<", "<V")
 set("n", "<Leader>sv", "<C-w>v")
 set("n", "<Leader>sh", "<C-w>s")
 set("n", "<Leader>se", "<C-w>=")
-set("n", "<Leader>q", ":quit<Return>", { silent = true })
-set("n", "<Leader>c", ":close<Return>", { silent = true })
+set("n", "<Leader>q", ":quit<Return>", opts)
+set("n", "<Leader>c", ":close<Return>", opts)
 
 -- Split resize
 set("n", "<C-up>", "<C-w>+")
@@ -29,23 +31,23 @@ set("n", "<C-down>", "<C-w>-")
 set("n", "<C-right>", "<C-w>>")
 set("n", "<C-left>", "<C-w><")
 
-set("n", "<C-\\>", ":MaximizerToggle<Return>", { silent = true })
+set("n", "<C-\\>", ":MaximizerToggle<Return>", opts)
 
 -- Increase / decrease
 set("n", "=", "<C-a>")
 set("n", "-", "<C-x>")
 
 -- File tree
-set("n", "<Leader>e", ":NvimTreeToggle<Return>", { silent = true })
+set("n", "<Leader>e", ":NvimTreeToggle<Return>", opts)
 
 -- Telescope
-set("n", "<Leader>ff", ":Telescope find_files<Return>", { silent = true })
-set("n", "<Leader>fg", ":Telescope live_grep<Return>", { silent = true })
-set("n", "<Leader>fs", ":Telescope grep_string<Return>", { silent = true })
-set("n", "<Leader>fb", ":Telescope buffers<Return>", { silent = true })
-set("n", "<Leader>fo", ":Telescope oldfiles<Return>", { silent = true })
+set("n", "<Leader>ff", ":Telescope find_files<Return>", opts)
+set("n", "<Leader>fg", ":Telescope live_grep<Return>", opts)
+set("n", "<Leader>fs", ":Telescope grep_string<Return>", opts)
+set("n", "<Leader>fb", ":Telescope buffers<Return>", opts)
+set("n", "<Leader>fo", ":Telescope oldfiles<Return>", opts)
 
 -- Buffers
-set("n", "H", ":BufferLineCyclePrev<Return>", { silent = true })
-set("n", "L", ":BufferLineCycleNext<Return>", { silent = true })
-set("n", "<Leader>x", ":bdelete<Return>", { silent = true })
+set("n", "H", ":BufferLineCyclePrev<Return>", opts)
+set("n", "L", ":BufferLineCycleNext<Return>", opts)
+set("n", "<Leader>x", ":bdelete<Return>", opts)
