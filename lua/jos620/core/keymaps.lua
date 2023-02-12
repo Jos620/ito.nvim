f49387ef26060db5250762b389fc65e9c1340e82
Mateus Ito <mateusito10@gmail.com>
2023-02-11 23:28:23 -0300

    chore: rename opts to silent

--- lua/jos620/core/keymaps.lua
+++ lua/jos620/core/keymaps.lua
@@ -1,10 +1,10 @@
 local set = vim.keymap.set
 
-local opts = { silent = true }
+local silent = { silent = true }
 
 -- Exit insert mode
 set("i", "jk", "<Esc>")
-set("i", "jj", "<Esc>:wa<Return>", opts)
+set("i", "jj", "<Esc>:wa<Return>", silent)
 
 -- Save
 set("n", "<C-s>", ":write<Return>")
@@ -17,7 +17,7 @@ set("v", "J", ":m '>+1<Return>gv=gv")
 set("v", "K", ":m '<-2<Return>gv=gv")
 
 -- Reload configuration
-set("n", "<Leader>r", ":ReloadConfig<Return>", opts)
+set("n", "<Leader>r", ":ReloadConfig<Return>", silent)
 
 -- Line indent with visual
 set("v", ">", ">gv")
@@ -27,8 +27,8 @@ set("v", "<", "<gv")
 set("n", "<Leader>sv", "<C-w>v")
 set("n", "<Leader>sh", "<C-w>s")
 set("n", "<Leader>se", "<C-w>=")
-set("n", "<Leader>q", ":quit<Return>", opts)
-set("n", "<Leader>x", ":close<Return>", opts)
+set("n", "<Leader>q", ":quit<Return>", silent)
+set("n", "<Leader>x", ":close<Return>", silent)
 
 -- Split resize
 set("n", "<C-up>", "<C-w>+")
@@ -36,25 +36,25 @@ set("n", "<C-down>", "<C-w>-")
 set("n", "<C-right>", "<C-w>>")
 set("n", "<C-left>", "<C-w><")
 
-set("n", "<C-m>", ":MaximizerToggle<Return>", opts)
+set("n", "<C-m>", ":MaximizerToggle<Return>", silent)
 
 -- Increase / decrease
 set("n", "=", "<C-a>")
 set("n", "-", "<C-x>")
 
 -- File tree
-set("n", "<Leader>e", ":NvimTreeFindFileToggle<Return>", opts)
+set("n", "<Leader>e", ":NvimTreeFindFileToggle<Return>", silent)
 
 -- Telescope
-set("n", "<Leader>ff", ":Telescope find_files<Return>", opts)
-set("n", "<Leader>fs", ":Telescope live_grep<Return>", opts)
-set("n", "<Leader>fg", ":Telescope git_status<Return>", opts)
-set("n", "<Leader>fb", ":Telescope buffers<Return>", opts)
-set("n", "<Leader>fo", ":Telescope oldfiles<Return>", opts)
-set("n", "<Leader>fk", ":Telescope keymaps<Return>", opts)
-set("n", "<Leader>fh", ":Telescope highlights<Return>", opts)
+set("n", "<Leader>ff", ":Telescope find_files<Return>", silent)
+set("n", "<Leader>fs", ":Telescope live_grep<Return>", silent)
+set("n", "<Leader>fg", ":Telescope git_status<Return>", silent)
+set("n", "<Leader>fb", ":Telescope buffers<Return>", silent)
+set("n", "<Leader>fo", ":Telescope oldfiles<Return>", silent)
+set("n", "<Leader>fk", ":Telescope keymaps<Return>", silent)
+set("n", "<Leader>fh", ":Telescope highlights<Return>", silent)
 
 -- Buffers
-set("n", "H", ":BufferLineCyclePrev<Return>", opts)
-set("n", "L", ":BufferLineCycleNext<Return>", opts)
-set("n", "<Leader>c", ":bdelete<Return>", opts)
+set("n", "H", ":BufferLineCyclePrev<Return>", silent)
+set("n", "L", ":BufferLineCycleNext<Return>", silent)
+set("n", "<Leader>c", ":bdelete<Return>", silent)
