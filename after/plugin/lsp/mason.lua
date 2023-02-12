5a3057b6e78753dfce895ea301a775e4ab8296a6
Mateus Ito <mateusito10@gmail.com>
2023-02-12 12:55:01 -0300

    fix: use lua_ls instead of sumneko_lua

--- after/plugin/lsp/mason.lua
+++ after/plugin/lsp/mason.lua
@@ -1,33 +1,33 @@
 local mason_status, mason = pcall(require, "mason")
 if not mason_status then
-	return
+  return
 end
 
 local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
-	return
+  return
 end
 
 local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
 if not mason_null_ls_status then
-	return
+  return
 end
 
 mason.setup()
 
 mason_lspconfig.setup({
-	ensure_installed = {
-		"tsserver",
-		"html",
-		"cssls",
-		"tailwindcss",
-		"sumneko_lua",
-	},
+  ensure_installed = {
+    "tsserver",
+    "html",
+    "cssls",
+    "tailwindcss",
+    "lua_ls",
+  },
 })
 
 mason_null_ls.setup({
-	ensure_installed = {
-		"eslint_d",
-		"stylua",
-	},
+  ensure_installed = {
+    "eslint_d",
+    "stylua",
+  },
 })
