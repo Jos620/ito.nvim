3b17e7d45a23fa33679857c4f071a8a5a9a2526c
Mateus Ito <mateusito10@gmail.com>
2023-02-13 20:38:01 -0300

    refactor: abbreviate api calls

--- lua/jos620/autocmd.lua
+++ lua/jos620/autocmd.lua
@@ -1,7 +1,9 @@
 local api = vim.api
+local augroup = api.nvim_create_augroup
+local autocmd = api.nvim_create_autocmd
 
-local tmux_augroup = api.nvim_create_augroup("TMUX", { clear = true })
-api.nvim_create_autocmd("BufRead, BufNewFile", {
+local tmux_augroup = augroup("TMUX", { clear = true })
+autocmd("BufRead, BufNewFile", {
   pattern = "*.conf",
   command = "set filetype=tmux",
   group = tmux_augroup,
