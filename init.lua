5a4fc9054902adb9d4f78891e0c2ec697383d2ba
Mateus Ito <mateusito10@gmail.com>
2023-02-13 21:30:52 -0300

    chore: update require order

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
-require("jos620.utils")
-require("jos620.autocmd")
-require("jos620.highlight")
-
 require("jos620.core.options")
 require("jos620.core.keymaps")
 require("jos620.core.plugins")
+
+require("jos620.utils")
+require("jos620.autocmd")
+require("jos620.highlight")
