f207eefaf7c6481668f846cbe21e6bb48220794a
Mateus Ito <mateusito10@gmail.com>
2023-03-06 11:47:54 -0300

    feat: add neovide config

--- init.lua
+++ init.lua
@@ -5,3 +5,7 @@ require("jos620.core.plugins")
 require("jos620.utils")
 require("jos620.autocmd")
 require("jos620.highlight")
+
+if vim.g.neovide then
+  require("jos620.neovide")
+end
---
+++ lua/jos620/neovide.lua
@@ -0,0 +1,11 @@
+-- Window options
+vim.o.guifont = "CaskaydiaCove NFM:h20"
+vim.g.neovide_remember_window_size = true
+vim.g.neovide_refresh_rate = 60
+vim.g.neovide_background_color = "#121212"
+
+-- Fix for alt key on MacOS
+vim.g.neovide_input_macos_alt_is_meta = true
+
+-- Animations
+vim.g.neovide_cursor_animation_length = 0.1
