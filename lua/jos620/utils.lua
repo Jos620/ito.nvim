015b7d353ad51f091d2e3df339b904dfee54e780
Mateus Ito <mateusito10@gmail.com>
2023-03-09 11:21:28 -0300

    feat(utils): reload `after/plugin` files

--- lua/jos620/utils.lua
+++ lua/jos620/utils.lua
@@ -1,5 +1,26 @@
 -- Reload configuration
 function _G.ReloadConfig()
+  -- Reload plugin configuration files
+  local function reload_files(path)
+    for _, file in ipairs(vim.fn.readdir(path)) do
+      local full_path = path .. "/" .. file
+
+      if vim.fn.isdirectory(full_path) == 1 then
+        reload_files(full_path)
+      elseif file:match("%.lua$") then
+        local load = loadfile(full_path)
+
+        if load then
+          load()
+        else
+          print("Error loading plugin file: " .. full_path)
+        end
+      end
+    end
+  end
+  reload_files(vim.fn.stdpath("config") .. "/after/plugin")
+
+  -- Reload other configuration files
   for name in pairs(package.loaded) do
     if name:match("^jos620") then
       package.loaded[name] = nil
