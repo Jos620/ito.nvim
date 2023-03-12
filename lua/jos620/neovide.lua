1a3295517427c03e0b23f9747f379df7d062a0e2
Mateus Ito <mateusito10@gmail.com>
2023-03-11 22:20:13 -0300

    feat: change font to CaskaydiaCove NFM

--- lua/jos620/neovide.lua
+++ lua/jos620/neovide.lua
@@ -1,5 +1,5 @@
 -- Window options
-vim.o.guifont = "FiraCode Nerd Font:h20"
+vim.o.guifont = "CaskaydiaCove NFM:h20"
 vim.g.neovide_remember_window_size = true
 vim.g.neovide_refresh_rate = 60
 vim.g.neovide_background_color = "#121212"
