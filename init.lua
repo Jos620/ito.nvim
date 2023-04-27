19a15aa5cafd97cef3ee664582593f041888ff9e
Mateus Ito <mateusito10@gmail.com>
2023-04-27 10:10:55 -0300

    refactor(neovide): remove redundant check

--- init.lua
+++ init.lua
@@ -5,7 +5,4 @@ require("jos620.core.plugins")
 require("jos620.utils")
 require("jos620.autocmd")
 require("jos620.highlight")
-
-if vim.g.neovide then
-  require("jos620.neovide")
-end
+require("jos620.neovide")
