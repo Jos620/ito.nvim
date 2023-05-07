a9428cd2b55704e0e951c838ab675808428db468
Mateus Ito <mateusito10@gmail.com>
2023-05-06 23:09:58 -0300

    fix(init): import order

--- init.lua
+++ init.lua
@@ -1,8 +1,9 @@
+require("jos620.utils")
+
 require("jos620.core.options")
 require("jos620.core.keymaps")
 require("jos620.core.plugins")
 
-require("jos620.utils")
 require("jos620.autocmd")
 require("jos620.highlight")
 require("jos620.neovide")
