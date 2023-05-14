f34bbf0e13bad00d2835cb7e06d9b67ce2210efc
Mateus Ito <mateusito10@gmail.com>
2023-05-13 22:40:18 -0300

    feat(plugins): add harpoon plugin

--- lua/jos620/core/plugins.lua
+++ lua/jos620/core/plugins.lua
@@ -66,6 +66,7 @@ return packer.startup(function(use)
   use("justinmk/vim-sneak")
   use("jesseleite/vim-noh")
   use("karb94/neoscroll.nvim")
+  use("ThePrimeagen/harpoon")
 
   -- Comments
   use("numToStr/Comment.nvim")
