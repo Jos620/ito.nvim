bf3a03ed59e23c94648cebaec4d9520f13ba181d
Mateus Ito <mateusito10@gmail.com>
2023-05-15 16:23:22 -0300

    feat(commands): add harpoon commands

--- lua/jos620/commands.lua
+++ lua/jos620/commands.lua
@@ -1,1 +1,4 @@
 vim.cmd("command! ReloadConfig lua ReloadConfig()")
+
+vim.cmd("command! Harpoon lua require('harpoon.ui').toggle_quick_menu()")
+vim.cmd("command! HarpoonAdd lua require('harpoon.mark').add_file()")
