a74741c9d8cd2db34c584a0a7634aa10256470ec
Mateus Ito <mateusito10@gmail.com>
2023-05-30 23:11:25 -0300

    fix(highlight): defer custom highlight setup

--- lua/jos620/highlight.lua
+++ lua/jos620/highlight.lua
@@ -1,34 +1,36 @@
 local colors = require("jos620.colors")
 
-local link_to_normal_groups = {
-  "NvimTreeFolderName",
-  "NvimTreeOpenedFolderName",
-  "NvimTreeEmptyFolderName",
-  "NvimTreeIndentMarker",
-  "NvimTreeRootFolder",
-  "packerWorking",
-}
+vim.defer_fn(function()
+  local link_to_normal_groups = {
+    "NvimTreeFolderName",
+    "NvimTreeOpenedFolderName",
+    "NvimTreeEmptyFolderName",
+    "NvimTreeIndentMarker",
+    "NvimTreeRootFolder",
+    "packerWorking",
+  }
 
-for _, group in ipairs(link_to_normal_groups) do
-  vim.cmd("highlight! link " .. group .. " Normal")
-end
+  for _, group in ipairs(link_to_normal_groups) do
+    vim.cmd("highlight! link " .. group .. " Normal")
+  end
 
-local green_fg_groups = {
-  "NvimTreeFolderIcon",
-  "NvimTreeOpenedFolderIcon",
-  "NvimTreeClosedFolderIcon",
-  "NvimTreeFolderArrowIcon",
-  "NvimTreeFileIcon",
-  "NvimTreeIndentMarker",
-  "NvimTreeGitDirty",
-  "NvimTreeGitStaged",
-  "NvimTreeGitMerge",
-  "NvimTreeGitNew",
-  "NvimTreeGitRenamed",
-  "NvimTreeGitDeleted",
-  "NvimTreeGitIgnored",
-}
+  local green_fg_groups = {
+    "NvimTreeFolderIcon",
+    "NvimTreeOpenedFolderIcon",
+    "NvimTreeClosedFolderIcon",
+    "NvimTreeFolderArrowIcon",
+    "NvimTreeFileIcon",
+    "NvimTreeIndentMarker",
+    "NvimTreeGitDirty",
+    "NvimTreeGitStaged",
+    "NvimTreeGitMerge",
+    "NvimTreeGitNew",
+    "NvimTreeGitRenamed",
+    "NvimTreeGitDeleted",
+    "NvimTreeGitIgnored",
+  }
 
-for _, group in ipairs(green_fg_groups) do
-  vim.cmd("highlight! " .. group .. " guifg=" .. colors.green)
-end
+  for _, group in ipairs(green_fg_groups) do
+    vim.cmd("highlight! " .. group .. " guifg=" .. colors.green)
+  end
+end, 0)
