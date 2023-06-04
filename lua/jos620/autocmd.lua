fa01327875635392714c4a1e399c9cb58f799519
Mateus Ito <mateusito10@gmail.com>
2023-06-04 13:56:05 -0300

    chore(autocmd): remove unused code

--- lua/jos620/autocmd.lua
+++ lua/jos620/autocmd.lua
@@ -8,19 +8,6 @@ autocmd("BufRead, BufNewFile", {
   group = tmux_augroup,
 })
 
-local auto_format_files = {
-  "*.dart",
-}
-
-local auto_format_augroup = augroup("AutoFormat", { clear = true })
-local auto_format_pattern = table.concat(auto_format_files, ",")
-
-autocmd("BufWritePre", {
-  pattern = auto_format_pattern,
-  command = "silent! lua vim.lsp.buf.formatting_sync()",
-  group = auto_format_augroup,
-})
-
 function _G.Setup()
   if vim.fn.argc() == 0 then
     vim.cmd("NvimTreeOpen")
