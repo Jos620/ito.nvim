b84b8346efd208ee272632d063f90eec8e87f157
Mateus Ito <mateusito10@gmail.com>
2023-06-04 19:05:55 -0300

    feat(flutter): setup flutter-tools lsp

--- after/plugin/lsp/languages/flutter.lua
+++ after/plugin/lsp/languages/flutter.lua
@@ -3,13 +3,22 @@ if not status then
   return
 end
 
+local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not cmp_nvim_lsp_status then
+  return
+end
+
 flutter_tools.setup({
-  dev_log = {
-    enabled = false,
+  ui = {
+    border = "rounded",
   },
   lsp = {
     on_attach = function(_, buffer)
       require("jos620.core.keymaps").setup_lsp_keymaps(buffer)
     end,
+    capabilities = cmp_nvim_lsp.default_capabilities(),
+  },
+  widget_guides = {
+    enabled = true,
   },
 })
