44b9297ea0c4d014d9068db9a96c2951a058957b
Mateus Ito <mateusito10@gmail.com>
2023-06-04 13:57:22 -0300

    refactor(autocmd): organize autocmds

--- lua/jos620/autocmd.lua
+++ lua/jos620/autocmd.lua
@@ -1,13 +1,6 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
-local tmux_augroup = augroup("TMUX", { clear = true })
-autocmd("BufRead, BufNewFile", {
-  pattern = "*.conf",
-  command = "set filetype=tmux",
-  group = tmux_augroup,
-})
-
 function _G.Setup()
   if vim.fn.argc() == 0 then
     vim.cmd("NvimTreeOpen")
@@ -17,4 +10,12 @@ end
 autocmd("VimEnter", {
   pattern = "*",
   command = "lua Setup()",
+})
+
+autocmd("BufRead, BufNewFile", {
+  pattern = "*.conf",
+  command = "set filetype=tmux",
+  group = augroup("TMUX", {
+    clear = true,
+  }),
 })
