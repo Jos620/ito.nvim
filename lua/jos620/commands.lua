6d78b3a23599ac90728cc7a9ee27dc8af6e365a6
Mateus Ito <mateusito10@gmail.com>
2023-06-09 11:14:25 -0300

    feat(commands): add NullLsFormat command

--- lua/jos620/commands.lua
+++ lua/jos620/commands.lua
@@ -2,3 +2,5 @@ vim.cmd("command! ReloadConfig lua ReloadConfig()")
 
 vim.cmd("command! Harpoon lua require('harpoon.ui').toggle_quick_menu()")
 vim.cmd("command! HarpoonAdd lua require('harpoon.mark').add_file()")
+
+vim.cmd("command! NullLsFormat lua NullLsFormat(vim.api.nvim_get_current_buf())")
