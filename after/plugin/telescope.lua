7250becf200c6c50bebe6bf59ee0e5ea81095cea
Mateus Ito <mateusito10@gmail.com>
2023-06-21 18:11:34 -0300

    feat(telescope): add split keymaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,7 +3,22 @@ if not telescope_status then
   return
 end
 
-telescope.setup()
+local actions = require("telescope.actions")
+
+local split_keymaps = {
+  ["<C-x>"] = false,
+  ["<C-v>"] = actions.select_vertical,
+  ["<C-b>"] = actions.select_horizontal,
+}
+
+telescope.setup({
+  defaults = {
+    mappings = {
+      i = split_keymaps,
+      n = split_keymaps,
+    },
+  },
+})
 
 if vim.fn.executable("fzf") == 1 then
   telescope.load_extension("fzf")
