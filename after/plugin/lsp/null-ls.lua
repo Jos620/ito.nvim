b3bb70bbab4f6a67cfea24c31e2c7c3867712e51
Mateus Ito <mateusito10@gmail.com>
2023-06-27 10:43:57 -0300

    fix(null-ls): create augroup only once

--- after/plugin/lsp/null-ls.lua
+++ after/plugin/lsp/null-ls.lua
@@ -40,6 +40,8 @@ local eslint_file_types = {
   "vue",
 }
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
   sources = {
     -- Spell checking
@@ -95,8 +97,6 @@ null_ls.setup({
   },
   on_attach = function(current_client, bufnr)
     if current_client.supports_method("textDocument/formatting") then
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
