522d2f16641791d87c4e945b18e58286c4ca003a
Mateus Ito <mateusito10@gmail.com>
2023-07-17 20:30:17 -0300

    style(nvim-tree): add min and max sizes to the window

--- after/plugin/ui/nvim-tree.lua
+++ after/plugin/ui/nvim-tree.lua
@@ -29,25 +29,25 @@ nvim_tree.setup({
       quit_on_focus_loss = false,
       open_win_config = function()
         local HEIGHT_RATIO = 0.8
+        local min_win_height = 10
+        local max_win_height = 20
+
         local WIDTH_RATIO = 0.75
+        local min_win_width = 40
+        local max_win_width = 60
 
         local screen_width = vim.opt.columns:get()
         local window_width = math.ceil(screen_width * WIDTH_RATIO)
         local screen_height = vim.opt.lines:get() - vim.opt.cmdheight:get()
         local window_height = math.ceil(screen_height * HEIGHT_RATIO)
 
+        window_height = math.max(min_win_height, math.min(max_win_height, window_height))
+        window_width = math.max(min_win_width, math.min(max_win_width, window_width))
+
         local position = {
           row = ((vim.opt.lines:get() - window_height) / 2) - vim.opt.cmdheight:get(),
           col = (screen_width - window_width) / 2,
         }
-
-        if window_width < 80 and window_width < screen_width / 2 then
-          window_width = 80
-        end
-
-        if window_height < 20 and window_height < screen_height / 2 then
-          window_height = 20
-        end
 
         return {
           border = "rounded",
