983eb7111764529d92ef32f9d3a3f374872e264a
Mateus Ito <mateusito10@gmail.com>
2023-08-09 12:31:34 -0300

    feat(plugins): install mason

---
+++ lua/jos620/plugins/lsp/mason/init.lua
@@ -0,0 +1,11 @@
+return {
+  "williamboman/mason.nvim",
+  config = function()
+    local mason_status, mason = pcall(require, "mason")
+    if not mason_status then
+      return
+    end
+
+    mason.setup()
+  end,
+}
