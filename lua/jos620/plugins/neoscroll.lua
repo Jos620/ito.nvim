930ef7fc38bb98a0bffc7dcb0a2a14a52921adab
Mateus Ito <mateusito10@gmail.com>
2023-08-09 10:59:55 -0300

    feat(plugins): install neoscroll.nvim

---
+++ lua/jos620/plugins/neoscroll.lua
@@ -0,0 +1,15 @@
+return {
+  "karb94/neoscroll.nvim",
+  config = function()
+    local status, neoscroll = pcall(require, "neoscroll")
+
+    if not status then
+      return
+    end
+
+    neoscroll.setup({
+      mappings = { "<C-u>", "<C-d>", "zt", "zz", "zb" },
+      hide_cursor = false,
+    })
+  end,
+}
