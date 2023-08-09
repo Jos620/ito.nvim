7cc794e24f361d01fe01c5b716f36fc0589e9657
Mateus Ito <mateusito10@gmail.com>
2023-08-09 11:50:31 -0300

    feat(plugins): install todo-comments.nvim

---
+++ lua/jos620/plugins/todo-comments.lua
@@ -0,0 +1,36 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    local status, todo = pcall(require, "todo-comments")
+
+    if not status then
+      return
+    end
+
+    local colors = require("jos620.colors")
+
+    todo.setup({
+      colors = {
+        error = { colors.red },
+        warning = { colors.yellow },
+        info = { colors.blue },
+        hint = { colors.green },
+        default = { colors.white },
+        test = { colors.cyan },
+      },
+
+      keywords = {
+        FIX = { color = "error" },
+        TODO = { color = "info" },
+        HACK = { color = "warning" },
+        WARN = { color = "warning" },
+        NOTE = { color = "hint" },
+      },
+    })
+  end,
+}
+
+-- TODO: Do something
