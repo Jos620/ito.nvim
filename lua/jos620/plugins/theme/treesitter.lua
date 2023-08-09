7e6fbbcc79d9c4723d85c88f7f8df2bc39233661
Mateus Ito <mateusito10@gmail.com>
2023-08-09 12:49:50 -0300

    feat(plugins): install nvim-treesitter

---
+++ lua/jos620/plugins/theme/treesitter.lua
@@ -0,0 +1,33 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  config = function()
+    local status, treesitter = pcall(require, "nvim-treesitter.configs")
+    if not status then
+      return
+    end
+
+    treesitter.setup({
+      highlight = { enable = true },
+      indent = { enable = true },
+      autotag = { enable = true },
+
+      ensure_installed = {
+        "typescript",
+        "javascript",
+        "tsx",
+        "html",
+        "css",
+        "json",
+        "markdown",
+        "markdown_inline",
+        "lua",
+        "vim",
+        "yaml",
+        "toml",
+        "bash",
+      },
+
+      auto_install = true,
+    })
+  end,
+}
