02983fa35d5f2c1601860e92182a99b99fc66c7f
Mateus Ito <mateusito10@gmail.com>
2023-08-09 16:09:51 -0300

    chore(autocmd): remove unused VimEnter autocmd

--- lua/jos620/autocmd.lua
+++ lua/jos620/autocmd.lua
@@ -1,11 +1,6 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
-autocmd("VimEnter", {
-  pattern = "*",
-  command = "lua Setup()",
-})
-
 autocmd("BufRead, BufNewFile", {
   pattern = "*.conf",
   command = "set filetype=tmux",
