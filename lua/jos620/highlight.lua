0592b73e3f836d816b2002cbc3b44138a7a5160a
Mateus Ito <mateusito10@gmail.com>
2023-08-09 15:58:19 -0300

    perf(highlight): remove unnecessary defer_fn

--- lua/jos620/highlight.lua
+++ lua/jos620/highlight.lua
@@ -8,43 +8,41 @@ local function link(group, link_to)
   vim.cmd("highlight! link " .. group .. " " .. link_to)
 end
 
-vim.defer_fn(function()
-  local link_to_normal_groups = {
-    "NvimTreeFolderName",
-    "NvimTreeOpenedFolderName",
-    "NvimTreeEmptyFolderName",
-    "NvimTreeIndentMarker",
-    "NvimTreeRootFolder",
-    "packerWorking",
-    "harpoonwindow",
-  }
+local link_to_normal_groups = {
+  "NvimTreeFolderName",
+  "NvimTreeOpenedFolderName",
+  "NvimTreeEmptyFolderName",
+  "NvimTreeIndentMarker",
+  "NvimTreeRootFolder",
+  "packerWorking",
+  "harpoonwindow",
+}
 
-  for _, group in ipairs(link_to_normal_groups) do
-    link(group, "Normal")
-  end
+for _, group in ipairs(link_to_normal_groups) do
+  link(group, "Normal")
+end
 
-  local green_fg_groups = {
-    "NvimTreeFolderIcon",
-    "NvimTreeOpenedFolderIcon",
-    "NvimTreeClosedFolderIcon",
-    "NvimTreeFolderArrowIcon",
-    "NvimTreeFileIcon",
-    "NvimTreeIndentMarker",
-    "NvimTreeGitDirty",
-    "NvimTreeGitStaged",
-    "NvimTreeGitMerge",
-    "NvimTreeGitNew",
-    "NvimTreeGitRenamed",
-    "NvimTreeGitDeleted",
-    "NvimTreeGitIgnored",
-    "harpoonborder",
-  }
+local green_fg_groups = {
+  "NvimTreeFolderIcon",
+  "NvimTreeOpenedFolderIcon",
+  "NvimTreeClosedFolderIcon",
+  "NvimTreeFolderArrowIcon",
+  "NvimTreeFileIcon",
+  "NvimTreeIndentMarker",
+  "NvimTreeGitDirty",
+  "NvimTreeGitStaged",
+  "NvimTreeGitMerge",
+  "NvimTreeGitNew",
+  "NvimTreeGitRenamed",
+  "NvimTreeGitDeleted",
+  "NvimTreeGitIgnored",
+  "harpoonborder",
+}
 
-  for _, group in ipairs(green_fg_groups) do
-    hi(group, { fg = colors.green })
-  end
+for _, group in ipairs(green_fg_groups) do
+  hi(group, { fg = colors.green })
+end
 
-  hi("ColorColumn", { bg = colors.darkgray })
-  hi("BufferLineIndicatorVisible", { bg = colors.black })
-  hi("NvimTreeSpecialFile", { fg = colors.green, bold = true })
-end, 0)
+hi("ColorColumn", { bg = colors.darkgray })
+hi("BufferLineIndicatorVisible", { bg = colors.black })
+hi("NvimTreeSpecialFile", { fg = colors.green, bold = true })
