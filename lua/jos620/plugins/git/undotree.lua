ab53aaf8b8ef61a40217da203daebc9e494964c7
Mateus Ito <mateusito10@gmail.com>
2023-08-10 16:02:46 -0300

    fix(plugins): load undotree on commands

--- lua/jos620/plugins/git/undotree.lua
+++ lua/jos620/plugins/git/undotree.lua
@@ -1,6 +1,12 @@
 return {
   "mbbill/undotree",
   event = "BufRead",
+  cmd = {
+    "UndotreeFocus",
+    "UndotreeHide",
+    "UndotreeShow",
+    "UndotreeToggle",
+  },
   config = function()
     vim.g.undotree_WindowLayout = 3
   end,
