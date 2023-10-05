66bd0e5e3f185c9a3fca2f1d75ec85cb03706aef
Mateus Ito <mateusito10@gmail.com>
2023-10-05 08:29:52 -0300

    feat(plugins): add conform.nvim

---
+++ lua/jos620/plugins/lsp/formatting/conform.lua
@@ -0,0 +1,21 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local status, conform = pcall(require, "conform")
+    if not status then
+      return
+    end
+
+    conform.setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      },
+    })
+  end,
+}
