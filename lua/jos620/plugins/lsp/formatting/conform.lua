51c6256c626b420977c47dd1feae5e65dce3c6b9
Mateus Ito <mateusito10@gmail.com>
2023-10-05 09:03:16 -0300

    chore(conform): use eslint_d and prettierd for js/ts

--- lua/jos620/plugins/lsp/formatting/conform.lua
+++ lua/jos620/plugins/lsp/formatting/conform.lua
@@ -9,10 +9,10 @@ return {
 
     conform.setup({
       formatters_by_ft = {
-        typescript = { { "prettierd", "prettier" } },
-        javascript = { { "prettierd", "prettier" } },
-        typescriptreact = { { "prettierd", "prettier" } },
-        javascriptreact = { { "prettierd", "prettier" } },
+        typescript = { "eslint_d", "prettierd" },
+        javascript = { "eslint_d", "prettierd" },
+        typescriptreact = { "eslint_d", "prettierd" },
+        javascriptreact = { "eslint_d", "prettierd" },
         lua = { "stylua" },
       },
       format_on_save = {
