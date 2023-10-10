367df3701178bdf91d5f4f7e3953873b2fbe0d2c
Mateus Ito <mateusito10@gmail.com>
2023-10-10 17:47:58 -0300

    feat(lsp): use eslint_d linter for vue files

--- lua/jos620/plugins/lsp/formatting/lint.lua
+++ lua/jos620/plugins/lsp/formatting/lint.lua
@@ -12,6 +12,7 @@ return {
       javascript = { "eslint_d" },
       typescriptreact = { "eslint_d" },
       javascriptreact = { "eslint_d" },
+      vue = { "eslint_d" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
