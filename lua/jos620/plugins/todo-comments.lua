49cc691901319cab52b7a0c0275a1c0536559799
Mateus Ito <mateusito10@gmail.com>
2023-11-09 00:40:31 -0300

    fix(plugins): lazy load todo-comments on BufRead

--- lua/jos620/plugins/todo-comments.lua
+++ lua/jos620/plugins/todo-comments.lua
@@ -4,7 +4,7 @@ return {
     "nvim-lua/plenary.nvim",
   },
   cmd = { "TodoLocList", "TodoQuickFix", "TodoTelescope" },
-  lazy = true,
+  event = "BufRead",
   config = function()
     local status, todo = pcall(require, "todo-comments")
 
