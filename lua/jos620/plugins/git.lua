da90eb3fe25cfe9f85ac29611570b75e0eef70b2
Mateus Ito <mateusito10@gmail.com>
2023-11-21 09:56:33 -0300

    refactor(git): move git-messenger to git list

--- lua/jos620/plugins/git.lua
+++ lua/jos620/plugins/git.lua
@@ -43,4 +43,16 @@ return {
       })
     end,
   },
+
+  { -- Authoring
+    "rhysd/git-messenger.vim",
+    cmd = "GitMessenger",
+    config = function()
+      vim.g.git_messenger_no_default_mappings = true
+      vim.g.git_messenger_always_into_popup = true
+      vim.g.git_messenger_floating_win_opts = {
+        border = "rounded",
+      }
+    end,
+  },
 }
