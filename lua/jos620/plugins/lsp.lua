fc8dfe6e3b5368afa8182bc62436938d496dde34
Mateus Ito <mateusito10@gmail.com>
2023-11-21 10:51:37 -0300

    refactor(lsp): move conform to lsp list

--- lua/jos620/plugins/lsp.lua
+++ lua/jos620/plugins/lsp.lua
@@ -210,6 +210,45 @@ return {
         require("jos620.core.keymaps").setup_linting_keymaps(lint)
       end,
     },
+
+    {
+      "stevearc/conform.nvim",
+      event = { "BufReadPre", "BufNewFile" },
+      config = function()
+        local conform = require("conform")
+
+        local javascript_formatters = { "eslint_d" }
+        local prettier_configs = {
+          ".prettierrc",
+          ".prettierrc.yaml",
+        }
+
+        if RootHasFile(prettier_configs) then
+          table.insert(javascript_formatters, "prettierd")
+        end
+
+        conform.setup({
+          formatters_by_ft = {
+            typescript = javascript_formatters,
+            javascript = javascript_formatters,
+            typescriptreact = javascript_formatters,
+            javascriptreact = javascript_formatters,
+            vue = javascript_formatters,
+            lua = { "stylua" },
+            css = { "stylelint" },
+            scss = { "stylelint" },
+            html = { "prettierd" },
+          },
+          format_on_save = {
+            lsp_fallback = true,
+            async = false,
+            timeout_ms = 750,
+          },
+        })
+
+        require("jos620.core.keymaps").setup_formatting_keymaps(conform)
+      end,
+    },
   },
 
   { -- Code completion
