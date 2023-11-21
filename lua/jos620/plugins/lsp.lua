4a85bf79560add90ffceb4896854d68ff65a5cc6
Mateus Ito <mateusito10@gmail.com>
2023-11-21 10:50:02 -0300

    refactor(lsp): move lint to lsp list

--- lua/jos620/plugins/lsp.lua
+++ lua/jos620/plugins/lsp.lua
@@ -178,6 +178,40 @@ return {
     },
   },
 
+  { -- Lint / format
+    {
+      "mfussenegger/nvim-lint",
+      event = { "BufReadPre", "BufNewFile" },
+      config = function()
+        local lint = require("lint")
+
+        lint.linters_by_ft = {
+          typescript = { "eslint_d" },
+          javascript = { "eslint_d" },
+          typescriptreact = { "eslint_d" },
+          javascriptreact = { "eslint_d" },
+          vue = { "eslint_d" },
+          css = { "stylelint" },
+          scss = { "stylelint" },
+        }
+
+        local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+        vim.api.nvim_create_autocmd({
+          "BufWritePost",
+          "BufEnter",
+          "InsertLeave",
+        }, {
+          group = lint_augroup,
+          callback = function()
+            lint.try_lint()
+          end,
+        })
+
+        require("jos620.core.keymaps").setup_linting_keymaps(lint)
+      end,
+    },
+  },
+
   { -- Code completion
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
