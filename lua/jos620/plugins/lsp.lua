cc271d4da82513036f5e86e6ce873a129f61e10b
Mateus Ito <mateusito10@gmail.com>
2023-11-21 10:53:01 -0300

    refactor(lsp): move lspsaga to lsp list

--- lua/jos620/plugins/lsp.lua
+++ lua/jos620/plugins/lsp.lua
@@ -178,6 +178,51 @@ return {
     },
   },
 
+  { -- LSP UI
+    "glepnir/lspsaga.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+    },
+    event = "BufReadPre",
+    config = function()
+      local lspsaga = require("lspsaga")
+
+      lspsaga.setup({
+        ui = {
+          border = "rounded",
+        },
+        move_in_saga = {
+          prev = "<C-k>",
+          next = "<C-j>",
+        },
+        finder = {
+          keys = {
+            toggle_or_open = "<Return>",
+            vsplit = "v",
+            split = "b",
+          },
+        },
+        definition = {
+          edit = "<Return>",
+          vsplit = "v",
+          split = "b",
+        },
+        symbol_in_winbar = {
+          enable = false,
+        },
+        beacon = {
+          enable = false,
+        },
+        lightbulb = {
+          enable = false,
+        },
+        hover = {
+          open_link = "o",
+        },
+      })
+    end,
+  },
+
   { -- Lint / format
     {
       "mfussenegger/nvim-lint",
