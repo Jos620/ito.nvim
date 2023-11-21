89f87aaa988e520353ec7750d4e5db65a129a299
Mateus Ito <mateusito10@gmail.com>
2023-11-21 10:37:23 -0300

    chore(plugins): remove unnecessary configuration

--- lua/jos620/plugins/lsp.lua
+++ lua/jos620/plugins/lsp.lua
@@ -187,6 +187,7 @@ return {
       "hrsh7th/cmp-nvim-lsp",
       "onsails/lspkind.nvim",
       "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
     },
     config = function()
       vim.opt.completeopt = "menu,menuone,noselect"
