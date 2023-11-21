bb0e178005cc497798caba707d62363c43e170ee
Mateus Ito <mateusito10@gmail.com>
2023-11-21 09:57:20 -0300

    refactor(git): move undotree to git list

--- lua/jos620/plugins/git.lua
+++ lua/jos620/plugins/git.lua
@@ -55,4 +55,17 @@ return {
       }
     end,
   },
+
+  { -- File history
+    "mbbill/undotree",
+    cmd = {
+      "UndotreeFocus",
+      "UndotreeHide",
+      "UndotreeShow",
+      "UndotreeToggle",
+    },
+    config = function()
+      vim.g.undotree_WindowLayout = 3
+    end,
+  },
 }
