3f3ff86da8ed0278b0ada28d8267231c34896445
Mateus Ito <mateusito10@gmail.com>
2023-11-21 09:47:18 -0300

    refactor(theme): move treesitter to theme list

---
+++ lua/jos620/plugins/theme.lua
@@ -0,0 +1,45 @@
+return {
+  { -- Treesitter
+    "nvim-treesitter/nvim-treesitter",
+    cmd = {
+      "TSInstall",
+      "TSUninstall",
+      "TSUpdate",
+      "TSUpdateSync",
+      "TSInstallInfo",
+      "TSInstallSync",
+      "TSInstallFromGrammar",
+    },
+    event = "User FileOpened",
+    config = function()
+      local status, treesitter = pcall(require, "nvim-treesitter.configs")
+      if not status then
+        return
+      end
+
+      treesitter.setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        autotag = { enable = true },
+
+        ensure_installed = {
+          "typescript",
+          "javascript",
+          "tsx",
+          "html",
+          "css",
+          "json",
+          "markdown",
+          "markdown_inline",
+          "lua",
+          "vim",
+          "yaml",
+          "toml",
+          "bash",
+        },
+
+        auto_install = true,
+      })
+    end,
+  },
+}
