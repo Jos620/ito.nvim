dda1bd06ce1413899a15ef5eee803908201f4875
Mateus Ito <mateusito10@gmail.com>
2023-12-06 14:24:52 -0300

    feat(utils): type methods

--- lua/jos620/utils.lua
+++ lua/jos620/utils.lua
@@ -1,14 +1,24 @@
-function _G.RootHasFile(...)
-  local files = Flatten({ ... })
+local M = {}
 
-  for _, file in ipairs(files) do
+--- Checks if any of the specified files exist in the root directory.
+---@param files string[]  -- List of file names
+---@return boolean
+function M.RootHasFile(files)
+  local flatFiles = M.Flatten({ files })
+
+  for _, file in ipairs(flatFiles) do
     if vim.fn.filereadable(vim.fn.getcwd() .. "/" .. file) == 1 then
       return true
     end
   end
+
+  return false
 end
 
-function _G.FileIsInWorkingDirectory(filepath)
+--- Checks if a given file path is within the working directory.
+---@param filepath string  -- File path to check
+---@return boolean
+function M.FileIsInWorkingDirectory(filepath)
   local cwd = vim.fn.getcwd()
 
   if filepath:sub(1, #cwd) == cwd then
@@ -18,7 +28,10 @@ function _G.FileIsInWorkingDirectory(filepath)
   return false
 end
 
-function _G.GetFilePathByMark(mark)
+--- Retrieves the file path associated with a specified mark.
+---@param mark string  -- Mark to search for
+---@return string|nil  -- File path or nil if not found
+function M.GetFilePathByMark(mark)
   local list = vim.fn.getmarklist()
 
   for _, item in ipairs(list) do
@@ -26,10 +39,14 @@ function _G.GetFilePathByMark(mark)
       return item.file
     end
   end
+
+  return nil
 end
 
--- Type conversion
-function _G.StringToBoolean(original)
+--- Converts a string or boolean to a boolean value.
+---@param original string|boolean  -- String or boolean input
+---@return boolean
+function M.StringToBoolean(original)
   if type(original) == "boolean" then
     return original
   end
@@ -43,13 +60,14 @@ function _G.StringToBoolean(original)
   return bool
 end
 
--- Table merge
-function _G.MergeTable(...)
-  local tables = { ... }
+--- Merges multiple tables into a single table.
+---@param tables table[]  -- Tables to merge
+---@return table  -- Merged table
+function M.MergeTables(tables)
   local result = {}
 
-  for _, table in ipairs(tables) do
-    for key, value in pairs(table) do
+  for _, tbl in ipairs(tables) do
+    for key, value in pairs(tbl) do
       result[key] = value
     end
   end
@@ -57,7 +75,11 @@ function _G.MergeTable(...)
   return result
 end
 
-function _G.Contains(tbl, value)
+--- Checks if a value is present in a table.
+---@param tbl table  -- Table to search in
+---@param value any   -- Value to search for
+---@return boolean
+function M.Contains(tbl, value)
   for _, current in pairs(tbl) do
     if current == value then
       return true
@@ -67,7 +89,10 @@ function _G.Contains(tbl, value)
   return false
 end
 
-function _G.Flatten(tbl)
+--- Flattens a nested table structure.
+---@param tbl any  -- Table to flatten
+---@return any[]   -- Flattened table
+function M.Flatten(tbl)
   local result = {}
 
   if type(tbl) ~= "table" then
@@ -76,7 +101,7 @@ function _G.Flatten(tbl)
 
   for _, value in ipairs(tbl) do
     if type(value) == "table" then
-      for _, current in ipairs(Flatten(value)) do
+      for _, current in ipairs(M.Flatten(value)) do
         table.insert(result, current)
       end
     else
@@ -87,26 +112,35 @@ function _G.Flatten(tbl)
   return result
 end
 
--- Range
-function _G.IsOneLine(range)
+--- Checks if a range represents a single line.
+---@param range number[]  -- Range to check
+---@return boolean
+function M.IsOneLine(range)
   return range[1] == range[3]
 end
 
-function _G.IsRangeEmptyOrInvalid(range)
-  if range[3] < range[1] or (IsOneLine(range) and range[4] <= range[2]) then
+--- Checks if a range is empty or invalid.
+---@param range number[]  -- Range to check
+---@return boolean
+function M.IsRangeEmptyOrInvalid(range)
+  if range[3] < range[1] or (M.IsOneLine(range) and range[4] <= range[2]) then
     return true
   end
 
   return false
 end
 
--- File / Directory
-function _G.IsDirectory(bufname)
+--- Checks if a buffer name represents a directory.
+---@param bufname string  -- Buffer name
+---@return boolean
+function M.IsDirectory(bufname)
   return vim.fn.isdirectory(vim.fn.fnamemodify(bufname, ":p"))
 end
 
--- Dependencies
-function _G.CheckDependency(executable)
+--- Checks if a specified executable is present.
+---@param executable string  -- Executable name
+---@return boolean
+function M.CheckDependency(executable)
   if vim.fn.executable(executable) == 1 then
     return true
   else
@@ -116,12 +150,17 @@ function _G.CheckDependency(executable)
   return false
 end
 
-function _G.CheckDependencies(executable_list)
-  for _, executable in ipairs(executable_list) do
-    if not CheckDependency(executable) then
+--- Checks if multiple dependencies are present.
+---@param executableList string[]  -- List of executable names
+---@return boolean
+function M.CheckDependencies(executableList)
+  for _, executable in ipairs(executableList) do
+    if not M.CheckDependency(executable) then
       return false
     end
   end
 
   return true
 end
+
+return M
