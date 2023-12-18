a1ebb17095746e90211af8a38c162a3edc6ec772
Mateus Ito <mateusito10@gmail.com>
2023-12-18 10:46:26 -0300

    chore(utils): add return descriptions to return types

--- lua/jos620/utils.lua
+++ lua/jos620/utils.lua
@@ -1,8 +1,8 @@
 local M = {}
 
---- Checks if any of the specified files exist in the root directory.
----@param files string[]  -- List of file names
----@return boolean
+---Checks if any of the specified files exist in the root directory.
+---@param files string[] -- List of file names
+---@return boolean       -- True if any file exists
 function M.RootHasFile(files)
   local flatFiles = M.Flatten({ files })
 
@@ -15,9 +15,9 @@ function M.RootHasFile(files)
   return false
 end
 
---- Checks if a given file path is within the working directory.
----@param filepath string  -- File path to check
----@return boolean
+---Checks if a given file path is within the working directory.
+---@param filepath string -- File path to check
+---@return boolean        -- True if file is in working directory
 function M.FileIsInWorkingDirectory(filepath)
   local cwd = vim.fn.getcwd()
 
@@ -28,9 +28,9 @@ function M.FileIsInWorkingDirectory(filepath)
   return false
 end
 
---- Retrieves the file path associated with a specified mark.
----@param mark string  -- Mark to search for
----@return string|nil  -- File path or nil if not found
+---Retrieves the file path associated with a specified mark.
+---@param mark string -- Mark to search for
+---@return string|nil -- File path or nil if not found
 function M.GetFilePathByMark(mark)
   local list = vim.fn.getmarklist()
 
@@ -43,9 +43,9 @@ function M.GetFilePathByMark(mark)
   return nil
 end
 
---- Converts a string or boolean to a boolean value.
----@param original string|boolean  -- String or boolean input
----@return boolean
+---Converts a string or boolean to a boolean value.
+---@param original string|boolean -- String or boolean input
+---@return boolean                -- Boolean equivalent
 function M.StringToBoolean(original)
   if type(original) == "boolean" then
     return original
@@ -60,9 +60,9 @@ function M.StringToBoolean(original)
   return bool
 end
 
---- Merges multiple tables into a single table.
----@param tables table[]  -- Tables to merge
----@return table  -- Merged table
+---Merges multiple tables into a single table.
+---@param tables table[] -- Tables to merge
+---@return table table   -- Merged table
 function M.MergeTables(tables)
   local result = {}
 
@@ -75,10 +75,10 @@ function M.MergeTables(tables)
   return result
 end
 
---- Checks if a value is present in a table.
----@param tbl table  -- Table to search in
----@param value any   -- Value to search for
----@return boolean
+---Checks if a value is present in a table.
+---@param tbl table -- Table to search in
+---@param value any -- Value to search for
+---@return boolean  -- True if value is present
 function M.Contains(tbl, value)
   for _, current in pairs(tbl) do
     if current == value then
@@ -89,9 +89,9 @@ function M.Contains(tbl, value)
   return false
 end
 
---- Flattens a nested table structure.
----@param tbl any  -- Table to flatten
----@return any[]   -- Flattened table
+---Flattens a nested table structure.
+---@param tbl any -- Table to flatten
+---@return any[]  -- Flattened table
 function M.Flatten(tbl)
   local result = {}
 
@@ -112,16 +112,16 @@ function M.Flatten(tbl)
   return result
 end
 
---- Checks if a range represents a single line.
----@param range number[]  -- Range to check
----@return boolean
+---Checks if a range represents a single line.
+---@param range number[] -- Range to check
+---@return boolean       -- True if range is one line
 function M.IsOneLine(range)
   return range[1] == range[3]
 end
 
---- Checks if a range is empty or invalid.
----@param range number[]  -- Range to check
----@return boolean
+---Checks if a range is empty or invalid.
+---@param range number[] -- Range to check
+---@return boolean       -- True if range is empty or invalid
 function M.IsRangeEmptyOrInvalid(range)
   if range[3] < range[1] or (M.IsOneLine(range) and range[4] <= range[2]) then
     return true
@@ -130,16 +130,16 @@ function M.IsRangeEmptyOrInvalid(range)
   return false
 end
 
---- Checks if a buffer name represents a directory.
----@param bufname string  -- Buffer name
----@return boolean
+---Checks if a buffer name represents a directory.
+---@param bufname string -- Buffer name
+---@return boolean       -- True if buffer is a directory
 function M.IsDirectory(bufname)
   return vim.fn.isdirectory(vim.fn.fnamemodify(bufname, ":p"))
 end
 
---- Checks if a specified executable is present.
----@param executable string  -- Executable name
----@return boolean
+---Checks if a specified executable is present.
+---@param executable string -- Executable name
+---@return boolean          -- True if executable is present
 function M.CheckDependency(executable)
   if vim.fn.executable(executable) == 1 then
     return true
@@ -150,9 +150,9 @@ function M.CheckDependency(executable)
   return false
 end
 
---- Checks if multiple dependencies are present.
----@param executableList string[]  -- List of executable names
----@return boolean
+---Checks if multiple dependencies are present.
+---@param executableList string[] -- List of executable names
+---@return boolean                -- True if all executables are present
 function M.CheckDependencies(executableList)
   for _, executable in ipairs(executableList) do
     if not M.CheckDependency(executable) then
