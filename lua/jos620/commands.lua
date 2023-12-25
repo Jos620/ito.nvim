f1153f46a6460392e8a734329f9670147a3cf028
Mateus Ito <mateusito10@gmail.com>
2023-12-25 10:09:37 -0300

    chore(commands): remove unused command

--- lua/jos620/commands.lua
+++ lua/jos620/commands.lua
@@ -7,5 +7,3 @@ end
 
 command("Harpoon", "require('harpoon.ui').toggle_quick_menu()")
 command("HarpoonAdd", "require('harpoon.mark').add_file()")
-
-command("NullLsFormat", "NullLsFormat(vim.api.nvim_get_current_buf())")
