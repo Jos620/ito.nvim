4284088c29aca820c61d599101543b2ee6d10577
Mateus Ito <mateusito10@gmail.com>
2024-03-04 07:32:09 -0300

    chore(telescope): remove unused file_browser plugin

--- lua/jos620/plugins/telescope.lua
+++ lua/jos620/plugins/telescope.lua
@@ -6,40 +6,12 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
-      "nvim-telescope/telescope-file-browser.nvim",
       "ThePrimeagen/harpoon",
     },
     event = "VeryLazy",
-    keys = {
-      {
-        "<Leader>F",
-        function()
-          local telescope = require("telescope")
-
-          local function telescope_buffer_dir()
-            return vim.fn.expand("%:p:h")
-          end
-
-          telescope.extensions.file_browser.file_browser({
-            path = "%:p:h",
-            cwd = telescope_buffer_dir(),
-            respect_gitignore = false,
-            hidden = true,
-            grouped = true,
-            previewer = false,
-            initial_mode = "normal",
-            layout_config = {
-              height = 40,
-            },
-          })
-        end,
-      },
-    },
     config = function()
       local telescope = require("telescope")
-
       local actions = require("telescope.actions")
-      local fb_actions = require("telescope").extensions.file_browser.actions
 
       local telescope_keymaps = {
         ["<C-x>"] = false,
@@ -75,35 +47,6 @@ return {
             ".DS_Store",
           },
         },
-        extensions = {
-          file_browser = {
-            theme = "dropdown",
-            hijack_netrw = true,
-            wrap_results = true,
-            mappings = {
-              ["n"] = {
-                ["a"] = fb_actions.create,
-                ["h"] = fb_actions.goto_parent_dir,
-                ["dd"] = fb_actions.remove,
-                ["/"] = function()
-                  vim.cmd("startinsert")
-                end,
-                ["<C-u>"] = function(bufnr)
-                  for _ = 1, 10 do
-                    actions.move_selection_previous(bufnr)
-                  end
-                end,
-                ["<C-d>"] = function(bufnr)
-                  for _ = 1, 10 do
-                    actions.move_selection_next(bufnr)
-                  end
-                end,
-                ["<PageUp>"] = actions.preview_scrolling_up,
-                ["<PageDown8>"] = actions.preview_scrolling_down,
-              },
-            },
-          },
-        },
       })
 
       keymaps.set("n", "<Leader>ff", ":Telescope find_files<Return>", "Find files")
@@ -117,11 +60,6 @@ return {
       local fzf_status = pcall(require, "telescope._extensions.fzf")
       if fzf_status and vim.fn.executable("fzf") == 1 then
         telescope.load_extension("fzf")
-      end
-
-      local file_browser_status = pcall(require, "telescope._extensions.file_browser")
-      if file_browser_status then
-        telescope.load_extension("file_browser")
       end
 
       local harpoon_status = pcall(require, "telescope._extensions.harpoon")
