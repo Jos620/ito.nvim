a222318a3ac6e2f06f8fa10e7d059991fcea19af
Mateus Ito <mateusito10@gmail.com>
2024-04-11 23:46:34 -0300

    perf(lazy): do not trigger FileOpened for oil buffers

--- lua/jos620/lazy.lua
+++ lua/jos620/lazy.lua
@@ -60,7 +60,9 @@ utils.create_autocmd({ "BufRead", "BufWinEnter", "BufNewFile" }, {
   callback = function(args)
     local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
 
-    if not (vim.fn.expand("%") == "" or buftype == "nofile") then
+    local is_oil_buffer = vim.api.nvim_buf_get_option(args.buf, "filetype") == "oil"
+
+    if not (vim.fn.expand("%") == "" or buftype == "nofile" or is_oil_buffer) then
       vim.api.nvim_del_augroup_by_name("_file_opened")
       vim.cmd("do User FileOpened")
     end
